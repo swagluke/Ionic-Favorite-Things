e2332f9e33f92899bc95095465424d328c8dc84c
Luke Zhang <zhangl@rose-hulman.edu>
2017-04-25 10:12:09 -0400

    Finished

--- IonicFavoriteThings/src/pages/home/home.ts
+++ IonicFavoriteThings/src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AngularFire } from "angularfire2";
 import * as firebase from 'firebase';
@@ -7,24 +7,36 @@ import * as firebase from 'firebase';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   public favoriteColor: string
-
+  public favoriteNumber = 0
   constructor(private af: AngularFire) {
 
   }
 
-  ngOnInt(): void {
-    firebase.database().ref().child("color").on("value", 
-    (snapshot: firebase.database.DataSnapshot) => {
-      this.favoriteColor = snapshot.val();
-    })
+  ngOnInit(): void {
+    firebase.database().ref().child("color").on("value",
+      (snapshot: firebase.database.DataSnapshot) => {
+        this.favoriteColor = snapshot.val();
+      })
+    firebase.database().ref().child("number").on("value",
+      (snapshot: firebase.database.DataSnapshot) => {
+        this.favoriteNumber = snapshot.val();
+      })
   }
 
   setColor(selectedColor: string) {
     console.log("TODO: Set the color in Firebase to " + selectedColor);
     firebase.database().ref().child("color").set(selectedColor);
+  }
+
+  changeNumberby(amount: number) {
+    this.setNumberInFirebase(amount + this.favoriteNumber);
+  }
+
+  setNumberInFirebase(newValue: number) {
+    firebase.database().ref().child("number").set(newValue);
   }
 
 }
